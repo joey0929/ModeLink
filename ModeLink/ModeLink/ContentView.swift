fc8be0c1827ef4cf3e4297f95c0301797790de48
joey <unjayintw@gmail.com>
2024-09-12 17:37:03 +0800

    add a clean file to this repo

---
+++ ModeLink/ModeLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ModeLink
+//
+//  Created by 池昀哲 on 2024/9/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Hello, world!")
+            
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ModeLink/ModeLink/ModeLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModeLinkApp.swift
+//  ModeLink
+//
+//  Created by 池昀哲 on 2024/9/12.
+//
+
+import SwiftUI
+
+@main
+struct ModeLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
